5809ad17a9728b2b117ed501f7062b4f3d4dcca0
barani1310-infosys <baranidharan@infosys.com>
2025-04-02 17:43:41 +0530

    update 4

--- Infra/eks-cluster.tf
+++ Infra/eks-cluster.tf
@@ -10,22 +10,49 @@ resource "aws_eks_cluster" "Example" {
   }
 }
 
+# IAM role for eks
+
 resource "aws_iam_role" "example" {
   name = "eks-cluster-example"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "eks.amazonaws.com"
+  tags = {
+    tag-key = "eks-cluster-demo"
+  }
+
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": [
+                    "eks.amazonaws.com"
+                ]
+            },
+            "Action": "sts:AssumeRole"
         }
-      }
     ]
-  })
 }
+POLICY
+}
+
+# resource "aws_iam_role" "example" {
+#   name = "eks-cluster-example"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "eks.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
+# }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
